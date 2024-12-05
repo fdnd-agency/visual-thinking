d17829900ea6bb5d9c0ad088a37a073882e1920c
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-12-05 15:04:30 +0100

    Button in onMount
    Heb de button en main JS in de onmount gezet en de main js in de static map verwijderd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,23 @@
     onMount(() => {
         console.log('De component is gemonteerd');
         console.log('De hoogte van de box:', box.clientHeight);
+
+        // console.log('js werkt')
+        let wrapper = document.querySelector('.wrapper')
+        let buttonThemeSwitcher = document.querySelector('.themeSwitcher')
+
+        buttonThemeSwitcher.addEventListener('click', themeSwitcher)
+
+        function themeSwitcher() {
+            console.log('werkt')
+            wrapper.classList.toggle('liquid-acid-theme')
+
+            if(this.innerHTML === "red pill") {
+                this.innerHTML = "blue pill"; // als er Frontend staat veranderd de innerHTML in Groen
+            } else {
+                this.innerHTML = "red pill"; // als er geen Frontend staat veranderd de innerHTML in Groen
+            }
+        }
     });
 
     export let x = 0.0;
