87af7f22334d70475ff9a473f840f8ba813a73bb
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-12-05 15:02:10 +0100

    OnMount
    OnMount:
    ahh dus onmount is eigenlijk de dom aanroepen, dus in gewone js ergens op de pagina zonder in een function te zetten. kreeg al veel 500 foutcodes in een component wanneer ik gewone console.log() deed buiten een function

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,43 @@
 <script>
+    import { onMount } from 'svelte';
     import Header from "$lib/molecules/header.svelte";
     import Footer from "$lib/molecules/footer.svelte";
 
-    export let x = 0.005;
+    onMount(() => {
+        console.log('De component is gemonteerd');
+        console.log('De hoogte van de box:', box.clientHeight);
+    });
+
+    export let x = 0.0;
+    export let a = 1;
 
     let box;
-
-    function scrollOffset(){
-        x = 0.00001 * box.scrollTop;
-        console.log(x);
-    }
     
+    function scrollOffset(){
+        x = 0.000001 * box.scrollTop
+        // boxHeight = box.clientHeight
+
+        a = 0.1 * box.scrollTop
+
+        if(x > 0.003) {
+            x = 0.003
+        }
+    }
 </script>
 
 <Header />
 
-<div bind:this={box} on:scroll={scrollOffset} class="wrapper liquid-acid-theme" style="--x: {x}">
+<div bind:this={box} on:scroll={scrollOffset} class="wrapper liquid-acid-theme" style="--x: {x} --a:{a}">
 <main><slot /></main>
 
-<g>
-    <svg viewBox="0 0 1728 852" fill="none" preserveAspectRatio="xMidYMin slice">
-        <defs>
-            <filter id="noise">
-                <feTurbulence  type="fractalNoise" id="turbulence" baseFrequency="{x}" numOctaves="1" result="noise" seed="0"/>
-                <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="300" />
-            </filter> 
-        </defs>
-    </svg>
-</g>
+<svg viewBox="0 0 1728 852" fill="none" preserveAspectRatio="xMidYMin slice">
+    <defs>
+        <filter id="noise">
+            <feTurbulence  type="fractalNoise" id="turbulence" baseFrequency="{x}" numOctaves="1" result="noise" seed="0"/>
+            <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="300" />
+        </filter>
+    </defs>
+</svg>
 
 
 <Footer />
@@ -47,30 +57,17 @@
         width: 100%;
         height: 100%;
         overflow-y: auto;
-        /* background: rgba(0,0,0, var(--x)); */
+        background: rgba(0,0,0, var(--a));
         /* filter: blur(calc(10px * var(--x))); */
-        /* background-color: blue;         */
+        /* background-color: blue; */
     }
 
-
-    
-    
     @keyframes colorChanceX {
         0% {
-            filter: blur(0px);
+            
         }
         100% {
-            filter: blur(calc(10px * var(--x)));
-        }
-    }
-    
-
-    @keyframes xAnimation {
-        from {
-            --x: 0.0;
-        }
-        to {
-            --x: 0.5;
+            
         }
     }
 
