c0da1b3edb63673d3c3b51e60afef7a0931be9fd
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-04-23 13:48:44 +0200

    Update Steps.svelte

--- src/lib/molecules/Steps.svelte
+++ src/lib/molecules/Steps.svelte
@@ -1,62 +1,65 @@
 <script>
-  export let steps;
+  export let data;
 </script>
 
-<section>
-  <!-- Loop through each step in the method -->
-  {#each steps as step, stepIndex}
-    <details class="step" open={stepIndex === 0}>
-      <summary class="step-title">{step.title}</summary>
-      <div class="step-content">
-        <!-- Check if visual content exists -->
-        {#if step.visual[0]}
-          <img src={step.visual[0].url} alt="image_voorbeeld" />
-        {/if}
-        <!-- Check if description is HTML or plain text -->
-        {#if typeof step.description === "object"}
-          <!-- i used {@html} for injecting HTML content without the html tags -->
-          {@html step.description.html}
-        {:else}
-          <!-- text description -->
-          <p>{step.description}</p>
-        {/if}
-      </div>
-    </details>
-  {/each}
-</section>
-
+<div class="steps">
+    <!-- Loop through each method -->
+    {#each data.methods as method, methodIndex}
+      <!-- Loop through each step in the method -->
+      {#each method.steps as step, stepIndex}
+        <details class="accordion-item" open={methodIndex === 0 && stepIndex === 0}>
+          <summary class="step-title">{step.title}</summary>
+          <div class="accordion-content">
+            <!-- Check if visual content exists -->
+            {#if step.visual[0]}
+              <img src={step.visual[0].url} alt="image_voorbeeld" />
+            {/if}
+            <!-- Check if description is HTML or plain text -->
+            {#if typeof step.description === "object"}
+              <!-- i used {@html} for injecting HTML content without the html tags -->
+             {@html step.description.html}
+            {:else}
+              <!-- text description -->
+              <p>{step.description}</p>
+            {/if}
+          </div>
+        </details>
+      {/each}
+    {/each}
+</div>
+  
 <style>
-  .step {
-    margin-bottom: 0.25rem;
+  .accordion-item {
+    margin-bottom: 5px;
     width: 100%;
     overflow: hidden;
   }
 
   .step-title {
-    font-family: var(--vtPrimaryFont);
-    background-color: var(--vtDarkBlue);
+    font-family: var(--font-family-primary);
+    background-color: var(--color-tertiary);
     border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    color: var(--vtWhite);
-    padding: 0.5rem;
+    box-shadow: 0 2px 4px hsla(0, 0%, 0%, 0.1);;
+    color: var(--color-primary);
+    padding: 10px;
     cursor: pointer;
     user-select: none;
   }
 
   .step-title:hover {
-    background-color: var(--vtYellow-80);
-    color: black;
+    background-color: var(--color-quinary-80);
+    color: var(--font-color);
   }
 
-  .step-content {
-    padding: 0.5rem;
-    background-color: var(--vtWhite);
-    border: 1px solid #ccc;
+  .accordion-content {
+    padding: 10px;
+    background-color: var(--color-primary);
+    border: 1px solid var(--color-septenary);
     border-radius: 8px;
-    margin-top: 0.25rem;
+    margin-top: 5px;
   }
 
-  .step-content img {
+  .accordion-content img {
     width: 100%;
     height: 100%;
   }
