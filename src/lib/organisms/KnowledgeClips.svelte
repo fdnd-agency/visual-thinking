19db04140b71196b2e80d99af0a3749294998dcc
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-04-23 13:53:35 +0200

    Update KnowledgeClips.svelte

--- src/lib/organisms/KnowledgeClips.svelte
+++ src/lib/organisms/KnowledgeClips.svelte
@@ -1,71 +1,60 @@
 <script>
-  import Breadcrumb from "$lib/atoms/breadcrumb.svelte";
+  import { Breadcrumb, Icons } from "$lib/index.js";
+  const iconSize = 50;
 
   let index = 0;
-
-  export let clips;
-
+  export let data;
   const nextButton = () => {
     index = index + 1;
   };
-
   const previousButton = () => {
     index = index - 1;
   };
+
 </script>
 
-<Breadcrumb titel="Kennisclips" backgroundColor="var(--vtSec-LightBlue)" />
-
-<main>
-  <h1 class="screenreader-only">Alle kennisclips</h1>
-  <section class="clips-container">
-    <button on:click={previousButton}>
-      <img
-        class="left-arrow"
-        src="./images/arrows.svg"
-        alt="Naar vorige video"
-      />
+<Breadcrumb titel="Kennisclips" bgc="var(--color-quaternary-80)" />
+<section>
+  <h1 class="sr-only">Alle kennisclips</h1>
+  <div class="clips-container">
+    <button on:click={previousButton} aria-label="Vorige Video">
+      <Icons name="arrowLeft" width={iconSize} height={iconSize}/>
     </button>
 
     <iframe
       class="youtubelink"
       title="kennisclips"
-      src={clips[index].youTubeLink}
-    ></iframe>
+      src={data.categories[index].youTubeLink}>
+    </iframe>
 
-    <button on:click={nextButton}>
-      <img
-        class="right-arrow"
-        src="./images/arrows.svg"
-        alt="Naar volgende video"
-      />
+    <button on:click={nextButton} aria-label="Volgende Video">
+      <Icons name="arrowRight" width={iconSize} height={iconSize}/>
     </button>
-  </section>
+  </div>
 
-  <section class="text-container">
-    <h2>{clips[index].title}</h2>
-    <p>{@html clips[index].content.html}</p>
-  </section>
-</main>
+  <div class="text-container">
+    <h2>{data.categories[index].title}</h2>
+    <p>{@html data.categories[index].content.html}</p>
+  </div>
+</section>
 
 <style>
-
   /* Header styling */
   h2 {
     font-size: 3.157rem;
-    font-family: var(--vtPrimaryFont);
-    color: var(--vtDarkBlue);
+    font-family: var(--font-family-primary);
+    color: var(--color-tertiary);
     text-align: center;
   }
-
+  
   /* Kennisclip teksten */
   p {
     line-height: 1.5rem;
-    font-family: var(--vtSecondaryFont);
+    font-family: var(--font-family-secondary);
     margin: 0 auto;
     max-width: 700px;
     text-transform: none;
-    padding-bottom: 3rem;
+    padding-bottom: 3em;
   }
 
   /* clips */
@@ -73,26 +62,17 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 3.3rem 0;
+    padding: 3.3em 0;
     margin:0 -1rem;
-    background-color: var(--vtGrey-10);
+    background-color: var(--color-septenary-40);
   }
 
   /* buttons */
   button {
     background-color: transparent;
     border: transparent;
-    padding: 2rem;
-  }
-
-  .right-arrow {
+    padding: 2em;
     cursor: pointer;
-    border: none;
-  }
-
-  .left-arrow {
-    cursor: pointer;
-    transform: rotate(180deg);
   }
 
   /* responsive clips */
@@ -104,10 +84,10 @@
   }
 
   /* desktop */
-  @media screen and (min-width: 56rem) {
+  @media screen and (min-width: 1120px) {
     .youtubelink {
-      width: 44.2rem;
-      height: 24.85rem;
+      width: 884px;
+      height: 497px;
     }
   }
 </style>
