6fb80d7157edf9697ae39e7cbc85fa98ff8de3b7
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-04-23 13:47:46 +0200

    Update Header.svelte

--- src/lib/molecules/Header.svelte
+++ src/lib/molecules/Header.svelte
@@ -1,13 +1,17 @@
 <script>
   import { page } from "$app/stores";
+  import { Icons } from "$lib/index.js";
 
-  // Used for the active state for the menu links
+  const logoSize = 80;
+
   let activeRoute;
-  $: activeRoute = $page.route.id; // 
+  $: activeRoute = $page.route.id;
 </script>
 
 <header>
-  <a href="/" class="logo"><img src="/images/visual-thinking-hbo-logo.svg"  alt="Home" /></a>
+  <a href="/" class="logo">
+    <Icons name="VisualThinkingLogo" width={logoSize} height={logoSize}/>
+  </a>
 
   <nav>
     <ul>
@@ -21,62 +25,56 @@
   </nav>
 </header>
 
-
 <style>
   a {
-    --background: var(--vtYellow);
-    color: var(--vtDarkBlue);
+    --background:var(--color-quinary);
+    color: var(--color-tertiary);
     text-decoration: none;
     cursor: pointer;
     font-size: clamp(.8rem, -1.5rem + 8vw, 1rem);
-    padding: .25rem;
+    padding:.25rem;
   }
 
   a.logo {
-    --background: var(--vtDarkBlue);
-    padding: 1rem .25rem 0;
-    margin-top: -.75rem;
+    --background:var(--color-tertiary);
+    padding:1rem .25rem 0;
+    margin-top:-.75rem;
     position:absolute;
-    top: 1rem;
-    left: 1rem;
-    z-index: 1;
-    transition: .25s;
+    top:1rem;
+    left:1rem;
+    z-index:1;
+    transition:.25s
   }
 
   a.logo:hover,
   a.logo:focus-visible {
     background: none;
-    outline: none;
-    scale: 1.1
-  }
-
-  a.logo img {
-    width: 3rem;
+    outline:none;
+    scale:1.1
   }
 
   nav {
-    --horizontal-spacing: 1rem;
-    padding: 1rem 1rem 1rem 3.5rem;
+    --horizontal-spacing:1rem;
+    padding:1rem 1rem 1rem 3.5rem;
     position: relative;
     overflow: hidden;
   }
 
   nav::before,
   nav::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 3.5rem;
+    content:"";
+    position:absolute;
+    top:0;
+    left:3.5rem;
     width: 3rem;
     height: 100%;
-
-    background: rgb(255,255,255);
+    background: var(--color-primary);
     background: linear-gradient(90deg, rgba(255,255,255,1) 40%, rgba(255,255,255,0) 100%);
   }
 
   nav::after {
-    left: auto;
-    right: -1rem;
+    left:auto;
+    right:-1rem;
     background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 50%);
   }
 
@@ -84,86 +82,82 @@
     list-style-type: none;
     display: flex;
     color: transparent;
-    font-family: var(--vtPrimaryFont);
+    font-family: var(--font-family-primary);
     font-weight: 700;
-    padding: 1rem;
-    margin: 0 1rem;
+    padding:1em;
+    margin: 0 1rem 0 1rem;
     align-items: center;
-    gap: 4vw;
-    overflow-x: auto;
-    width: 100%;
+    gap:4vw;
+    overflow-x:auto;
+    width:100%;
   }
 
   ul li a:hover,
   ul li a:focus-visible,
   ul li a.active {
-    --state-color: var(--vtSec-DarkBlue);
+    --state-color: var(--color-tertiary-80);
     text-decoration: underline;
-    text-decoration-color: var(--state-color);
-    text-underline-offset: 0.25rem;
-    outline: none;
+    text-decoration-color:var(--state-color);
+    text-underline-offset: 5px;
+    outline:none;
   }
 
   li:nth-child(1) a:hover,
   ul li:nth-child(1) a:focus-visible,
   ul li:nth-child(1) a.active {
-    --state-color: var(--vtYellow);
+    --state-color:var(--color-quinary);
   }
 
   li:nth-child(2) a:hover,
   ul li:nth-child(2) a:focus-visible,
   ul li:nth-child(2) a.active {
-    --state-color: var(--vtSec-DarkBlue);
+    --state-color: var(--color-tertiary-80);
   }
 
   li:nth-child(3) a:hover,
   ul li:nth-child(3) a:focus-visible,
   ul li:nth-child(3) a.active {
-    --state-color: var(--vtRed);
+    --state-color: var(--color-senary);
   }
 
   li:nth-child(4) a:hover,
   ul li:nth-child(4) a:focus-visible,
   ul li:nth-child(4) a.active {
-    --state-color: var(--vtSec-LightBlue);
+    --state-color: var(--color-quaternary);
   }
 
   li:nth-child(5) a:hover,
   ul li:nth-child(5) a:focus-visible,
   ul li:nth-child(5) a.active {
-    --state-color: var(--vtDarkBlue);
+    --state-color: var(--color-tertiary);
   }
 
   li:nth-child(6) a:hover,
   ul li:nth-child(6) a:focus-visible,
   ul li:nth-child(6) a.active {
-    --state-color: var(--vtGrey);
+    --state-color: var(--color-septenary);
   }
 
-  @media (min-width: 80rem) {
+  @media (min-width: 80em) {
     a.logo {
       position: absolute;
-      z-index: 1;
-      left: calc(50% - 2rem);
-      top: .5rem;
-    }
-
-    a.logo img {
-      width: 4rem;
+      z-index:1;
+      left:calc(50% - 40px);
+      top:.5rem;
     }
 
     nav {
-      --horizontal-spacing: 10vw;
-      padding-left: 1rem;
+      --horizontal-spacing:10vw;
+      padding-left:1rem;
     }
 
     nav::before,
     nav::after {
-      display: none;
+      display:none;
     }
 
     ul {
-      --spacing-logo: 6rem;
+      --spacing-logo:6rem;
       justify-content: center;
     }
 
