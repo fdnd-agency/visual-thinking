38c8b2b562596eed07d16e7757c1ed1e6de61997
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-04-27 15:27:22 +0200

    Update index.js
    netlify had some deployment issues, since the `index.js` wasn't structured right. There were some files that were written uppercase. but `index.js` didn't show that. I corrected that and changed all the components that the have a uppercase. Make this into a code convention at #302

--- src/lib/index.js
+++ src/lib/index.js
@@ -8,27 +8,27 @@ export { default as IconAuthor } from "./atoms/icons/IconAuthor.svelte";
 export { default as IconCategories } from "./atoms/icons/IconCategories.svelte";
 export { default as IconDuration } from "./atoms/icons/IconDuration.svelte";
 export { default as IconMaterials } from "./atoms/icons/IconMaterials.svelte";
-export { default as Breadcrumb } from "./atoms/breadcrumb.svelte";
-export { default as Hexagon } from "./atoms/hexagon.svelte";
+export { default as Breadcrumb } from "./atoms/Breadcrumb.svelte";
+export { default as Hexagon } from "./atoms/Hexagon.svelte";
 export { default as Icons } from "./atoms/Icons.svelte";
 
 
 // molecules
 export { default as Filter } from "./molecules/filter.svelte";
-export { default as Footer } from "./molecules/footer.svelte";
-export { default as Header } from "./molecules/header.svelte";
-export { default as Introduction } from "./molecules/introduction.svelte";
-export { default as Steps } from "./molecules/steps.svelte";
+export { default as Footer } from "./molecules/Footer.svelte";
+export { default as Header } from "./molecules/Header.svelte";
+export { default as Introduction } from "./molecules/Introduction.svelte";
+export { default as Steps } from "./molecules/Steps.svelte";
 export { default as StepsTags } from "./molecules/StepsTags.svelte";
 
 // organisms
-export { default as Articles } from "./organisms/articles.svelte";
-export { default as DrawingSpace } from "./organisms/drawingSpace.svelte";
-export { default as Homepage } from "./organisms/homepage.svelte";
-export { default as KnowledgeClips } from "./organisms/knowledgeClips.svelte";
-export { default as Methods } from "./organisms/methods.svelte";
-export { default as MethodHeader } from "./organisms/methodHeader.svelte";
-export { default as MiniCourses } from "./organisms/miniCourses.svelte";
+export { default as Articles } from "./organisms/Articles.svelte";
+export { default as DrawingSpace } from "./organisms/DrawingSpace.svelte";
+export { default as Homepage } from "./organisms/Homepage.svelte";
+export { default as KnowledgeClips } from "./organisms/KnowledgeClips.svelte";
+export { default as Methods } from "./organisms/Methods.svelte";
+export { default as MethodHeader } from "./organisms/MethodHeader.svelte";
+export { default as MiniCourses } from "./organisms/MiniCourses.svelte";
 
 
 
