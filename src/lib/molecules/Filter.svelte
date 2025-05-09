7864fa2745c6f45d455e50b574e3df440ad581c8
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-05-09 09:54:07 +0200

    styling labels

--- src/lib/molecules/Filter.svelte
+++ src/lib/molecules/Filter.svelte
@@ -13,11 +13,26 @@
 </fieldset>
 
 <style>
+
+  fieldset {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: .5rem;
+  }
   fieldset label {
     display: flex;
     flex-direction: row;
+    background-color: magenta;
+    padding: .5rem;
+    border-radius: 1rem;
   }
   fieldset label input[type="checkbox"] {
     display: none;
   }
+  
+  fieldset label input[type="checkbox"]:checked {
+    box-shadow: inset 2px 2px 3px rgb(0, 0, 0);
+    color: black;
+}
 </style>
