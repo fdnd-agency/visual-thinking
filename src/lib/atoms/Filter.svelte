451680541fceff73a4bea3613e2e4713c91e21c6
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-06-03 11:58:28 +0200

    restructure the filter
    #426 @DamianR2004 said the structutre wasn't right. So im fixing that here

--- src/lib/atoms/Filter.svelte
+++ src/lib/atoms/Filter.svelte
@@ -24,33 +24,34 @@
   <fieldset>
       <legend>Filter op categorie</legend>
     {#each categories as category}
-      <label for={category.slug} >
-          {category.title}
-        <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            width={width} 
-            height={height} 
-            viewBox="0 0 384 512"
-            class = "close-button"
-          >
+      <div>
+        <label for={category.slug} >
+            {category.title}
+          <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width={width} 
+              height={height} 
+              viewBox="0 0 384 512"
+              class = "close-button"
+            >
 
-          <path 
-            fill={color} 
-            d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-          />
-        </svg>
+            <path 
+              fill={color} 
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            />
+          </svg>
+        </label>
 
         <input 
-          type="checkbox"
-          id={category.slug}
-          name="filter"
-          value={category.slug}
-          onchange={() => {
-            form.requestSubmit();
-          }}
-        />
-
-      </label>
+            type="checkbox"
+            id={category.slug}
+            name="filter"
+            value={category.slug}
+            onchange={() => {
+              form.requestSubmit();
+            }}
+          />
+      </div>
       {/each}
     </fieldset>
     <button type="submit" class="filter-button">toepassen</button>
@@ -88,12 +89,11 @@
     padding-inline: 0.75em;
   }
 
-  form fieldset label {
+  form fieldset div {
     display: flex;
     align-items: center;
     flex-direction: row-reverse;
     gap: .5rem;
-    padding: 0.5rem .8rem ;
     width: fit-content;
 
     cursor: pointer;
@@ -108,8 +108,7 @@
       background-color: var(--color-septenary-40);
       border-bottom: .4em solid var(--color-quinary-80);
     }
-
-    & input[type="checkbox"] {
+      & input[type="checkbox"] {
       border: 1em solid var(--color-quinary);
       background-color: var(--color-quinary);
       width: .8rem;
@@ -118,12 +117,11 @@
       &::before {
         border: .1rem solid var(--color-quinary);
         background-color: var(--color-primary);
-
       }
     }
-  }
+  } 
   
-  form fieldset label:has(input:checked) {
+  form fieldset div:has(input:checked) {
     background-color: var(--color-septenary-40);
     border-bottom: .4em solid var(--color-quinary);
   }
@@ -187,7 +185,7 @@
     }
   }
 
-    .js-on label:has(input:checked) {
+    .js-on div:has(input:checked) {
     background-color: var(--color-quinary);
 
     .close-button {
@@ -195,7 +193,7 @@
     }
   }
 
-  .js-on label:has(input[type="checkbox"]:focus-visible) {
+  .js-on div:has(input[type="checkbox"]:focus-visible) {
   outline: var(--focus-outline);
   outline-offset: var(--focus-offset);
   border-radius: 1rem;
