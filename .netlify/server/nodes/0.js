135e114f0547cc0bc6abe65f65371d3e0db3ee14
DamianR04 <damian.romeijn@hva.nl>
2025-06-05 14:02:05 +0200

    DELETE NETLIFY FOLDER

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.C21FKBYr.js","_app/immutable/chunks/Bwoi2yR4.js","_app/immutable/chunks/q4Dn0bqy.js","_app/immutable/chunks/CbvezQth.js","_app/immutable/chunks/oBaFlM7i.js","_app/immutable/chunks/0gV53gZ9.js","_app/immutable/chunks/DMonA8zZ.js"];
-export const stylesheets = ["_app/immutable/assets/MiniCourses.3LJvtqbu.css"];
-export const fonts = [];
