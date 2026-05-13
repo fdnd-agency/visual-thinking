b1b45d6b1178b5263bb141358a3f64a52e19bb3c
Quin487 <qufox487@outlook.com>
2026-05-13 15:30:38 +0200

    feat: Now retrieves homepage data from directus. #500

---
+++ src/lib/molecules/Introduction.svelte
@@ -0,0 +1,41 @@
+<script>
+  let { page } = $props();
+  const {title, content} = page;
+
+</script>
+
+<section class="intro">
+  <h1>{title}</h1>
+  <p>{content}</p>
+</section>
+
+<style>
+  section {
+    max-width: var(--text-max-width);
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 1.7rem;
+  }
+  
+  @media screen and (min-width: 36rem) {
+    section {
+          max-width: 75%;
+      }
+      h1 {
+          font-size: 3.157rem;
+          text-align: center;
+      }
+  }
+
+  @media screen and (min-width: 60rem) {
+    section {
+          max-width: 32rem;
+      }
+      h1 {
+          font-size: 3.157rem;
+          text-align: center;
+      }
+  }
+</style>
