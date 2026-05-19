a8172ec095b08a3ff8649cb1f71957e7274d9034
Chaim <chaim.cadeau@hva.nl>
2026-05-19 13:20:49 +0200

    [Feature]#509 migrated kennisclip page from hygrap to directus.

--- src/routes/kennisclips/+page.server.js
+++ src/routes/kennisclips/+page.server.js
@@ -1,35 +1,60 @@
 import { gql } from "graphql-request";
 import { hygraph } from "$lib/utils/hygraph.js";
+import { directus } from "../../lib/utils/directus";
 
 
 
 export const load = async () => {
-  const query = gql`
-    query AllCategories {
-      page(where: {id: "cmhdhsj6agpqo06uylzxne5vd"}) {
-        title
-        content {
-          html
-        }
-      }
-      categories {
-        slug
-        title
-        youTubeLink
-        introduction
-        content {
-          html
-        }
-      }
-    }
-  `;
+  // const query = gql`
+  //   query AllCategories {
+  //     page(where: {id: "cmhdhsj6agpqo06uylzxne5vd"}) {
+  //       title
+  //       content {
+  //         html
+  //       }
+  //     }
+  //     categories {
+  //       slug
+  //       title
+  //       youTubeLink
+  //       introduction
+  //       content {
+  //         html
+  //       }
+  //     }
+  //   }
+  // `;
 
-  const data = await hygraph.request(query);
+
+  const directusquery = `
+  query AllCategories {
+    adconnect_kennisclip {
+      title
+    }
+
+    adconnect_kennisclip_category {
+      slug
+      title
+      youTubeLink
+    }
+  }
+`;
+
+
+  // const data = await hygraph.request(query);
+  let data
+  try {
+    data = await directus.query(directusquery);
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+    console.log(error.errors[0].extensions)
+  }
 
   console.log(data);
 
   return {
-    page: data.page,
-    clips: data.categories
+    page: data.adconnect_kennisclip,
+    clips: data.adconnect_kennisclip_category
   };
 };
