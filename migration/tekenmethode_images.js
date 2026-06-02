5b86e9abcf687c8d661133f1d0c8a2ec2e4d12fc
Quin487 <qufox487@outlook.com>
2026-06-02 14:08:09 +0200

    accidentally pushed my own session token. Luckily it rotates.

--- migration/tekenmethode_images.js
+++ migration/tekenmethode_images.js
@@ -44,7 +44,7 @@ methods.forEach((method) => {
 newTekenMethodes.forEach(async (newTekenMethode, i) => {
   if (i == 3) {
     try {
-      const postNewMethods = await fetch(`${directusUrl}/items/vt_tekenmethodes`, { method: "post", body: JSON.stringify(newTekenMethode), headers: {"Authorization": " Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjBjN2QwN2Q0LWQzYjYtNDRjNi1iYjM1LTlhOWEzMmYwOGQ3NCIsInJvbGUiOiI1MWVlNmI5ZS0wZmY5LTQyOTYtOGVkZC0xMzlkN2Q0OGM4YzIiLCJhcHBfYWNjZXNzIjp0cnVlLCJhZG1pbl9hY2Nlc3MiOnRydWUsInNlc3Npb24iOiJrS3pVTHY0amw2Qk9sbEdsdU1zcTVCTUJQVFVVTFV6Zk5hWWd2VHdIYUZjVUw2YUhvNTkxd2dYZzlqOVVRT2ZCIiwiaWF0IjoxNzgwMzIwMTQzLCJleHAiOjE3ODA0MDY1NDMsImlzcyI6ImRpcmVjdHVzIn0.VHpcBP7eFHpLQm7f7cq7257jxZa-HJAnTU_wDuovLLM", "Content-Type": "application/json"} });
+      const postNewMethods = await fetch(`${directusUrl}/items/vt_tekenmethodes`, { method: "post", body: JSON.stringify(newTekenMethode), headers: {"Authorization": " your token", "Content-Type": "application/json"} });
       console.log(postNewMethods.ok)
       console.log(postNewMethods);
       return;
