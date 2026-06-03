73b9a757083be8a9b68f1e5aea06585155d14778
Quin487 <qufox487@outlook.com>
2026-06-03 16:34:09 +0200

    [TEST] Tests if the h1 in a kennisclip is present #516

--- test/cms/e2e/kennisclip.spec.js
+++ test/cms/e2e/kennisclip.spec.js
@@ -2,9 +2,14 @@
 import { test, expect } from '@playwright/test';
 
 test('kennisclip has title', async ({ page }) => {
-    const slug = "Onderzoeken-en-begrijpen"
-    await page.goto('/kennisclips/' + slug);
+    const slug = 'onderzoeken-en-begrijpen';
 
-    // Expect a title "to contain" a substring.
+    await page.goto(`/kennisclips/${slug}`);
+    console.log(await page.content())
+
+    const h1 = page.locator('h1');
+
+    await expect(h1).toBeVisible();
+    await expect(h1).toContainText('Onderzoeken');
     await expect(page).toHaveTitle(/Visual Thinking/);
 });
