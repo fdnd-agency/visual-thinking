132ef613a2996200f59c3c4db979d8cdcdce256f
Chaim <chaim.cadeau@hva.nl>
2026-06-04 15:16:33 +0200

    Patch(kennisclips): #509 breadcrumd didnt show on the page now fixed.

--- src/routes/kennisclips/+page.svelte
+++ src/routes/kennisclips/+page.svelte
@@ -1,10 +1,9 @@
 <script>
   import { Breadcrumb, Introduction, KnowledgeClips } from "$lib/index.js";
   let { data } = $props();
-  const {page, clips} = data
-
+  const { page, clips } = data;
 </script>
 
-<Breadcrumb titel="Kennisclips" backgroundColor="var(--color-quaternary)" />
-<Introduction {page}/>
+<Breadcrumb title="Kennisclips" backgroundColor="var(--color-quaternary)" />
+<Introduction {page} />
 <KnowledgeClips {clips} />
